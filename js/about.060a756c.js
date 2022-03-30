9673fce5820ef1cc4997a99f7f6117a9cdccee5a
supersonictw <supersonic@livemail.tw>
2022-03-30 15:29:04 +0000

    Deploying to gh-pages from @ web-tech-tw/openchat@7ef2a95a177ab564ebb331fd566302d3cd027b60 🚀

--- js/about.060a756c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkopenchat_vue"]=self["webpackChunkopenchat_vue"]||[]).push([[443],{5830:function(e,t,n){n.r(t),n.d(t,{default:function(){return r}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},a=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],s=n(1001),c={},l=(0,s.Z)(c,u,a,!1,null,null,null),r=l.exports}}]);
-//# sourceMappingURL=about.060a756c.js.map
